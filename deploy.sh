b01132a8d24ca72bb7a619f857fc9372b2853333
caofayou <2017954256@qq.com>
2026-03-09 20:11:15 +0800

    feat: 更新部署脚本和构建命令

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:haixiangyan/resume.git master:gh-pages
+git push -f git@github.com:cfy1126/resume.git master:gh-pages
 
 cd -
